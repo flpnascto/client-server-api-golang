e5d38f77aaed51b819a5d443acba9849f97e9063
Felipe Nascimento <flpnascto@gmail.com>
2024-01-17 23:00:24 -0300

    add client go

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
